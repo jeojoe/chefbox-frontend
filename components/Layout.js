e6b3c8190e0740b7e7092074daf0a1238c6732f4
Jirat Onaree <jirat.onaree@gmail.com>
2017-09-17 13:22:48 +0700

    Add normalize.css

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
+import { stylesheet, classNames as c } from './Layout.css';
 
 const Layout = ({ children, title }) => (
   <div>
@@ -7,7 +8,13 @@ const Layout = ({ children, title }) => (
       <title>{ title }</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link href="https://fonts.googleapis.com/css?family=Mitr:300,500" rel="stylesheet" />
+      <link href="/static/normalize.css" rel="stylesheet" />
+      <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
     </Head>
+    <nav id={c.nav}>
+      sdkfjdslfsdkf
+    </nav>
     { children }
   </div>
 );
