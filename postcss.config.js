d39ec189bfaccc231f53c933faa294fa6fc03d57
Jirat Onaree <jirat.onaree@gmail.com>
2017-09-18 10:50:53 +0700

    Add postcss-nested

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 module.exports = {
   plugins: [
+    require('postcss-nested'),
     require('postcss-cssnext')(),
     require('postcss-modules')({
       generateScopedName: '[local]-[hash:base64:5]'
