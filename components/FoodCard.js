3af1941874da3ec3632cad5ee618357dbf2edfe2
Jirat Onaree <jirat.onaree@gmail.com>
2017-10-21 22:36:50 +0700

    Styling on food card

--- components/FoodCard.js
+++ components/FoodCard.js
@@ -12,7 +12,7 @@ const FoodCard = ({ food }) => (
       <div className="image" style={{ backgroundImage: 'url(https://media.blueapron.com/recipes/2511/c_main_dish_images/1503683201-7-0065-0427/925_2PP_Half-Chicken_82663_WEB_Center_high_menu_thumb.jpg)' }} />
       <div className="content">
         <div style={{ height: '80px', marginBottom: '20px' }}>
-          <h2 style={{ maxHeight: '54px', overflow: 'hidden' }}>กระเพราหมูกรอบ{Math.random() > 0.5 && 'sdfsdfsdfs dsdfsdfs sdff fsdfsdfsdfsf'}</h2>
+          <h2 style={{ maxHeight: '54px', overflow: 'hidden' }}>สปาเก็ตตี้ คาโบนาร่า {Math.random() > 0.5 && 'sdfsdfsdfs dsdfsdfs sdff fsdfsdfsdfsf'}</h2>
           <h5>280 ฿</h5>
         </div>
         <div className="details">
