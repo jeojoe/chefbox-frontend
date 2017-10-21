01f7902a6038541b231ce9009aeead5b6f44167f
Jirat Onaree <jirat.onaree@gmail.com>
2017-10-21 22:37:13 +0700

    Add instruction card

---
+++ components/InstructionCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from 'next/head';
+import stylesheet from '../styles/components/InstructionCard.css';
+
+const InstructionCard = ({ instruction }) => (
+  <div className="instruction-card">
+    <Head>
+      <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+    </Head>
+    <img src="/static/instruction-1.png" alt="Chefbox Instruction" />
+    <h3>1. เตรียมเส้น สปาเกตตี้</h3>
+    <p>ต้มน้ำให้เดือดใส่เกลือและน้ำมันมะกอกเล็กน้อย ตามด้วยเส้นลงไป ต้ม 7-8 นาที หรือ จนเส้นสุก แล้วนำขึ้นพักไว้</p>
+  </div>
+);
+
+export default InstructionCard;
--- components/index.js
+++ components/index.js
@@ -2,10 +2,12 @@ import Footer from './Footer';
 import Layout from './Layout';
 import FoodCard from './FoodCard';
 import CTASection from './CTASection';
+import InstructionCard from './InstructionCard';
 
 export {
   Footer,
   Layout,
   FoodCard,
   CTASection,
+  InstructionCard,
 };
