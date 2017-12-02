a498267a6fdf79c02c68fda51796898b04ee2018
Jirat Onaree <jirat.onaree@gmail.com>
2017-12-02 17:16:20 +0700

    try to unuglify about lol

--- pages/about.js
+++ pages/about.js
@@ -10,15 +10,17 @@ export default () => (
     </Head>
     <Layout title="About | Chef Box Delivery : Ready to cook right at your home.">
       <div className="container">
-        <div className="row center-xs header">
-          <div className="col-xs-12 col-md-8">
-            <img src="/static/logo.png" alt="Chefbox Logo" />
+        <div className="row header">
+          <div className="col-12 col-md-8">
+            <img src="/static/logo.png" alt="Chefbox Logo" className="mb-3" />
             <h1>Chefbox Delivery</h1>
-            <p>เราเชื่อว่าการทำอาหารนั้นสามารถทำให้เกิดได้ทั้ง ความสุข ความสนุก แรงบันดาลใจ การเรียนรู้ ความตั้งใจ ความภาคภูมิใจ และอีกมากมาย เพราะเราเองก็เป็นกลุ่มคนที่มีความหลงใหลในการทำอาหาร เราจึงอยากส่งต่อสิ่งเหล่านี้ให้คนอื่นให้ได้มากที่สุด</p>
+            <p>ประสบการณ์ใหม่ในการทำอาหาร เพื่อตัวคุณเองและคนที่คุณรัก
+     เราจัดส่งวัตถุดิบทุกอย่างที่จำเป็นสำหรับการทำอาหารมื้อพิเศษที่บ้านถึงมือคุณ โดยคัดเลือกวัตถุดิบชั้นเลิศ ในปริมาณที่พอดี ส่งมาให้พร้อมกับขั้นตอนการทำอย่างละเอียด ซึ่งคิดค้นขึ้นด้วยความใส่ใจ ทั้งสะอาด อร่อย และมีประโยชน์ พร้อมกับ มีทีมงานดูแลให้คำปรึกษาคุณจนจบขั้นตอนการประกอบอาหาร
+     ไม่ว่าคุณจะทำอาหารเป็นหรือไม่ คุณก็สามารถสร้างประสบการณ์ใหม่ไปกับเรา</p>
           </div>
         </div>
 
-        <div className="row detail" style={{ justifyContent: 'center' }}>
+        <div className="row detail">
           <div className="col-xs-12 col-md-8">
             <div className="row">
               <div className="col-xs-8 col-md-4">
@@ -26,7 +28,7 @@ export default () => (
                   Mission
                 </div>
               </div>
-              <div className="col-xs-12 col-md-7 col-md-offset-1">
+              <div className="col-xs-12 col-md-7">
                 <p className="head">To solve the problem of people who wanted to cook, make them enjoy cooking whenever they want.</p>
                 <p>เราจะแก้ปัญหาให้คนที่อยากทำอาหารเนื่องในโอกาสใดก็ตามได้ทำ สนุกกับการทำอาหารได้เมื่อพวกเค้าต้องการ</p>
               </div>
@@ -34,15 +36,15 @@ export default () => (
           </div>
         </div>
 
-        <div className="row detail" style={{ justifyContent: 'center' }}>
+        <div className="row detail">
           <div className="col-xs-12 col-md-8">
-            <div className="row" style={{ justifyContent: 'center' }}>
+            <div className="row">
               <div className="col-xs-8 col-md-4">
                 <div className="head-box">
                   What we do
                 </div>
               </div>
-              <div className="col-xs-12 col-md-7 col-md-offset-1">
+              <div className="col-xs-12 col-md-7">
                 <p className="head">เราจัดส่งเครื่องปรุงและวัตถุดิบสดใหม่ ที่ชั่งตวงไว้พอดีกับสูตรอาหารเด็ดๆ จากเชฟของเราถึงมือคุณ</p>
                 <p>โดยคุณสามารถสั่งสินค้าล่วงหน้าอย่างน้อย 1 วัน (ตัดออเดอร์ 6 โมงเย็น) และรับสินค้าในวันถัดไป โดยสามารถนัดเวลาจัดส่งกับ admin ของเราได้ทุกช่องทาง</p>
               </div>
@@ -50,7 +52,7 @@ export default () => (
           </div>
         </div>
         
-        <div className="row detail" style={{ justifyContent: 'center' }}>
+        <div className="row detail">
           <div className="col-xs-12 col-md-8">
             <div className="row">
               <div className="col-xs-8 col-md-4">
@@ -58,7 +60,7 @@ export default () => (
                   ช่องทางการสั่ง
                 </div>
               </div>
-              <div className="col-xs-12 col-md-7 col-md-offset-1">
+              <div className="col-xs-12 col-md-7">
                 <p className="head">Line@(@Chefboxdelivery) Inbox Facebook fan page (Chefbox.delivery)</p>
               </div>
             </div>
@@ -69,12 +71,6 @@ export default () => (
           <div className="col-xs-12 col-md-8">
             <p className="head">To solve the problem of people who wanted to cook, make them enjoy cooking whenever they want.</p>
             <p>เราจะแก้ปัญหาให้คนที่อยากทำอาหารเนื่องในโอกาสใดก็ตามได้ทำ สนุกกับการทำอาหารได้เมื่อพวกเค้าต้องการ</p>
-          </div>
-        </div>
-
-        <div className="row center-xs" style={{ marginTop: '50px' }}>
-          <div className="col-xs-12 col-md-6">
-            <CTASection />
           </div>
         </div>
       </div>
